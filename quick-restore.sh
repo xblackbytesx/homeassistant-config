5cb43b304e766304982fefbbc0095d19a4dacdfe
Fabio Bier <fabio@proxymail.nl>
2019-05-25 13:54:27 +0200

    Fixed the custom_updater script.

--- quick-restore.sh
+++ quick-restore.sh
@@ -9,7 +9,8 @@ rm lovelace-gen.py
 # Fetch desired custom components
 mkdir -p custom_components/radarr_upcoming_media
 mkdir -p custom_components/sonarr_upcoming_media
-wget https://raw.githubusercontent.com/custom-components/custom_updater/master/custom_components/custom_updater.py -O custom_components/custom_updater.py
+# wget https://raw.githubusercontent.com/custom-components/custom_updater/master/custom_components/custom_updater.py -O custom_components/custom_updater.py
+wget https://raw.githubusercontent.com/custom-components/custom_updater/4.2.19/custom_components/custom_updater.py -O custom_components/custom_updater.py
 wget https://raw.githubusercontent.com/custom-components/sensor.radarr_upcoming_media/master/custom_components/radarr_upcoming_media/sensor.py -O custom_components/radarr_upcoming_media/sensor.py
 wget https://raw.githubusercontent.com/custom-components/sensor.sonarr_upcoming_media/master/custom_components/sonarr_upcoming_media/sensor.py -O custom_components/sonarr_upcoming_media/sensor.py
 
@@ -20,7 +21,7 @@ mkdir -p www/custom-lovelace/lovelace-swipe-navigation
 wget https://raw.githubusercontent.com/custom-cards/tracker-card/master/tracker-card.js -O www/tracker-card.js
 wget https://raw.githubusercontent.com/custom-cards/upcoming-media-card/master/upcoming-media-card.js -O www/custom-lovelace/upcoming-media-card/upcoming-media-card.js
 wget https://raw.githubusercontent.com/ciotlosm/custom-lovelace/master/monster-card/monster-card.js -O www/custom-lovelace/monster-card/monster-card.js
-wget https://raw.githubusercontent.com/maykar/lovelace-swipe-navigation/master/lovelace-swipe-navigation.js -O www/custom-lovelace/lovelace-swipe-navigation/lovelace-swipe-navigation.js
+wget https://raw.githubusercontent.com/maykar/lovelace-swipe-navigation/master/swipe-navigation.js -O www/custom-lovelace/lovelace-swipe-navigation/swipe-navigation.js
 
 # Lovelace generator
 wget https://raw.githubusercontent.com/thomasloven/homeassistant-lovelace-gen/master/lovelace-gen.py
