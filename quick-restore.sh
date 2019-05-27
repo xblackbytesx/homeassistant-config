047c82e36dae1196fc41eea659100b1085888cb2
Fabio Bier <fabio@proxymail.nl>
2019-05-27 23:09:25 +0200

    Added the mini-graph-card custom card.

--- quick-restore.sh
+++ quick-restore.sh
@@ -21,6 +21,7 @@ mkdir -p www/custom-lovelace/lovelace-swipe-navigation
 mkdir -p www/custom-lovelace/lovelace-bar-card
 mkdir -p www/custom-lovelace/lovelace-fold-entity-row
 mkdir -p www/custom-lovelace/lovelace-card-tools
+mkdir -p www/custom-lovelace/lovelace-mini-graph-card
 wget https://raw.githubusercontent.com/custom-cards/tracker-card/master/tracker-card.js -O www/tracker-card.js
 wget https://raw.githubusercontent.com/custom-cards/upcoming-media-card/master/upcoming-media-card.js -O www/custom-lovelace/upcoming-media-card/upcoming-media-card.js
 wget https://raw.githubusercontent.com/ciotlosm/custom-lovelace/master/monster-card/monster-card.js -O www/custom-lovelace/monster-card/monster-card.js
@@ -28,6 +29,7 @@ wget https://raw.githubusercontent.com/maykar/lovelace-swipe-navigation/master/s
 wget https://raw.githubusercontent.com/custom-cards/bar-card/master/bar-card.js -O www/custom-lovelace/lovelace-bar-card/bar-card.js
 wget https://raw.githubusercontent.com/thomasloven/lovelace-card-tools/master/card-tools.js -O www/custom-lovelace/lovelace-card-tools/card-tools.js
 wget https://raw.githubusercontent.com/thomasloven/lovelace-fold-entity-row/master/fold-entity-row.js -O www/custom-lovelace/lovelace-fold-entity-row/fold-entity-row.js
+wget https://github.com/kalkih/mini-graph-card/releases/download/v0.4.2/mini-graph-card-bundle.js -O www/custom-lovelace/lovelace-mini-graph-card/mini-graph-card-bundle.js
 
 # Lovelace generator
 wget https://raw.githubusercontent.com/thomasloven/homeassistant-lovelace-gen/master/lovelace-gen.py
