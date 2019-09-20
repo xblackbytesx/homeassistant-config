5138a6c835a2611f4f2c9e3e71f1023de7814ed4
Fabio Bier <fabio@proxymail.nl>
2019-09-20 19:31:13 +0200

    Added new ssh-key.

--- restore-assets.sh
+++ restore-assets.sh
@@ -6,6 +6,7 @@ cp assets/ssh-config ~/.ssh/config
 cp assets/ssh-known_hosts ~/.ssh/known_hosts
 cp assets/id-github-master ~/.ssh/id-github-master
 cp assets/id-nighthawk-hassos ~/.ssh/id-nighthawk-hassos
+cp assets/id-hoth-master-hassos ~/.ssh/id-hoth-master-hassos
 cp assets/git-config ~/.gitconfig
 chmod 600 ~/.ssh/config ~/.ssh/id-*
 
