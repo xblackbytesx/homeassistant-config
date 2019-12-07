c266236900563e301baafd759a2eaba508465205
Fabio Bier <fabio@proxymail.nl>
2019-12-07 09:37:46 +0100

    Made restore-assets.sh script more sane.

--- restore-assets.sh
+++ restore-assets.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
 # Git and Github setup
-mkdir -p ~/.ssh
-cp assets/ssh-config ~/.ssh/config
-cp assets/ssh-known_hosts ~/.ssh/known_hosts
-cp assets/id-github-master ~/.ssh/id-github-master
-cp assets/id-nighthawk-hassos ~/.ssh/id-nighthawk-hassos
-cp assets/id-hoth-master-hassos ~/.ssh/id-hoth-master-hassos
+rm -rf ~/.ssh
+cp -Rp assets/ssh ~/.ssh
+
 cp assets/git-config ~/.gitconfig
-chmod 600 ~/.ssh/config ~/.ssh/id-*
+chmod -R 600 ~/.ssh/config ~/.ssh/private
 
 # Shell scripts
 rm -rf shell_scripts
