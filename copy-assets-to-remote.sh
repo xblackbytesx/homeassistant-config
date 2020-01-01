9e9a50494eb672271615be6ee5a16b4a87675cca
Fabio Bier <fabio@proxymail.nl>
2020-01-01 13:22:12 +0100

    Refactored the master bedroom button automation.

---
+++ copy-assets-to-remote.sh
@@ -0,0 +1,7 @@
+scp -r /home/fabio/projects/homeassistant-config/assets hassos:/config
+scp /home/fabio/projects/homeassistant-config/restore-assets.sh hassos:/config
+scp /home/fabio/projects/homeassistant-config/secrets.yaml hassos:/config
+scp /home/fabio/projects/homeassistant-config/nest.conf hassos:/config
+scp /home/fabio/projects/homeassistant-config/privkey.pem hassos:/config
+scp /home/fabio/projects/homeassistant-config/certificate.pem hassos:/config
+scp /home/fabio/projects/homeassistant-config/philips-hue.conf hassos:/config
