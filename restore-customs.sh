13bc6177d8ad152566ec7d44738e5db1ffdb122a
Fabio Bier <fabio@proxymail.nl>
2020-01-22 09:55:32 +0100

    Added echo output to the restore-customs script.

--- restore-customs.sh
+++ restore-customs.sh
@@ -6,60 +6,77 @@ rm -rf www/custom-lovelace
 rm www/tracker-card.js
 rm lovelace-gen.py
 
+echo "Installing lovelace_gen Component"
 mkdir -p custom_components/lovelace_gen
 wget https://raw.githubusercontent.com/thomasloven/hass-lovelace_gen/master/custom_components/lovelace_gen/__init__.py -O custom_components/lovelace_gen/__init__.py
 wget https://raw.githubusercontent.com/thomasloven/hass-lovelace_gen/master/custom_components/lovelace_gen/manifest.json -O custom_components/lovelace_gen/manifest.json
 
+echo "Installing RDW Component"
 mkdir -p custom_components/rdw
 wget https://raw.githubusercontent.com/eelcohn/home-assistant-rdw/master/__init__.py -O custom_components/rdw/__init__.py
 wget https://raw.githubusercontent.com/eelcohn/home-assistant-rdw/master/sensor.py -O custom_components/rdw/sensor.py
 wget https://raw.githubusercontent.com/eelcohn/home-assistant-rdw/master/manifest.json -O custom_components/rdw/manifest.json
 
+echo "Installing Radarr Component"
 mkdir -p custom_components/radarr_upcoming_media
 wget https://raw.githubusercontent.com/custom-components/sensor.radarr_upcoming_media/master/custom_components/radarr_upcoming_media/__init__.py -O custom_components/radarr_upcoming_media/__init__.py
 wget https://raw.githubusercontent.com/custom-components/sensor.radarr_upcoming_media/master/custom_components/radarr_upcoming_media/sensor.py -O custom_components/radarr_upcoming_media/sensor.py
 wget https://raw.githubusercontent.com/custom-components/sensor.radarr_upcoming_media/master/custom_components/radarr_upcoming_media/manifest.json -O custom_components/radarr_upcoming_media/manifest.json
 
+echo "Installing Sonarr Component"
 mkdir -p custom_components/sonarr_upcoming_media
 wget https://raw.githubusercontent.com/custom-components/sensor.sonarr_upcoming_media/master/custom_components/sonarr_upcoming_media/__init__.py -O custom_components/sonarr_upcoming_media/__init__.py
 wget https://raw.githubusercontent.com/custom-components/sensor.sonarr_upcoming_media/master/custom_components/sonarr_upcoming_media/sensor.py -O custom_components/sonarr_upcoming_media/sensor.py
 wget https://raw.githubusercontent.com/custom-components/sensor.sonarr_upcoming_media/master/custom_components/sonarr_upcoming_media/manifest.json -O custom_components/sonarr_upcoming_media/manifest.json
 
+echo "Installing ZigBee NetworkMap Component"
 git clone git@github.com:rgruebel/ha_zigbee2mqtt_networkmap.git custom_components/zigbee_networkmap
 mv custom_components/zigbee_networkmap/custom_components/zigbee2mqtt_networkmap custom_components/
 rm -rf custom_components/zigbee_networkmap
 
+echo "Installing Garbage Pickup Component"
 cp -Rp assets/custom_components/garbage_pickup custom_components/
 
+echo "Installing Decluttering Card"
 mkdir -p www/custom-lovelace/decluttering-card
 wget https://github.com/custom-cards/decluttering-card/raw/master/dist/decluttering-card.js -O www/custom-lovelace/decluttering-card/decluttering-card.js
 
+echo "Installing Upcoming Media Card"
 mkdir -p www/custom-lovelace/upcoming-media-card
 wget https://raw.githubusercontent.com/custom-cards/upcoming-media-card/master/upcoming-media-card.js -O www/custom-lovelace/upcoming-media-card/upcoming-media-card.js
 
+echo "Installing Auto-Entities Card"
 mkdir -p www/custom-lovelace/lovelace-auto-entities
 wget https://raw.githubusercontent.com/thomasloven/lovelace-auto-entities/master/auto-entities.js -O www/custom-lovelace/lovelace-auto-entities/auto-entities.js
 
+echo "Installing Swipe Navigation Card"
 mkdir -p www/custom-lovelace/lovelace-swipe-navigation
 wget https://raw.githubusercontent.com/maykar/lovelace-swipe-navigation/master/swipe-navigation.js -O www/custom-lovelace/lovelace-swipe-navigation/swipe-navigation.js
 
+echo "Installing Bar Card"
 mkdir -p www/custom-lovelace/lovelace-bar-card
 wget https://raw.githubusercontent.com/custom-cards/bar-card/master/bar-card.js -O www/custom-lovelace/lovelace-bar-card/bar-card.js
 
+echo "Installing Fold-entity Card"
 mkdir -p www/custom-lovelace/lovelace-fold-entity-row
 wget https://raw.githubusercontent.com/thomasloven/lovelace-fold-entity-row/master/fold-entity-row.js -O www/custom-lovelace/lovelace-fold-entity-row/fold-entity-row.js
 
+echo "Installing Card Tools"
 mkdir -p www/custom-lovelace/lovelace-card-tools
 wget https://raw.githubusercontent.com/thomasloven/lovelace-card-tools/master/card-tools.js -O www/custom-lovelace/lovelace-card-tools/card-tools.js
 
+echo "Installing Mini Graph Card"
 mkdir -p www/custom-lovelace/lovelace-mini-graph-card
 wget https://github.com/kalkih/mini-graph-card/releases/download/v0.9.0-beta/mini-graph-card-bundle.js -O www/custom-lovelace/lovelace-mini-graph-card/mini-graph-card-bundle.js
 
+echo "Installing Mini Media Player Card"
 mkdir -p www/custom-lovelace/lovelace-mini-media-player
 wget https://github.com/kalkih/mini-media-player/releases/download/v1.5.2/mini-media-player-bundle.js -O www/custom-lovelace/lovelace-mini-media-player/mini-media-player-bundle.js
 
+echo "Installing Simple Thermostat Card"
 mkdir -p www/custom-lovelace/simple-thermostat
 wget https://github.com/nervetattoo/simple-thermostat/releases/download/0.33.1/simple-thermostat.js -O www/custom-lovelace/simple-thermostat/simple-thermostat.js
 
+echo "Installing ZigBee NetworkMap Card"
 mkdir -p www/custom-lovelace/zigbee2mqtt-networkmap
 wget https://github.com/azuwis/zigbee2mqtt-networkmap/releases/download/v0.6.0/zigbee2mqtt-networkmap.js -O www/custom-lovelace/zigbee2mqtt-networkmap/zigbee2mqtt-networkmap.js
