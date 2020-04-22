29accf4a37e853ac74777369d9fff8e133a7b740
Fabio Bier <fabio@proxymail.nl>
2020-04-22 12:17:30 +0200

    Improved scenes card layout.

--- restore-customs.sh
+++ restore-customs.sh
@@ -53,9 +53,9 @@ echo "Installing Swipe Navigation Card"
 mkdir -p www/custom-lovelace/lovelace-swipe-navigation
 wget https://raw.githubusercontent.com/maykar/lovelace-swipe-navigation/master/swipe-navigation.js -O www/custom-lovelace/lovelace-swipe-navigation/swipe-navigation.js
 
-echo "Installing Swipe Card"
-mkdir -p www/custom-lovelace/swipe-card
-wget https://raw.githubusercontent.com/bramkragten/swipe-card/master/dist/swipe-card.js -O www/custom-lovelace/swipe-card/swipe-card.js
+# echo "Installing Swipe Card"
+# mkdir -p www/custom-lovelace/swipe-card
+# wget https://raw.githubusercontent.com/bramkragten/swipe-card/master/dist/swipe-card.js -O www/custom-lovelace/swipe-card/swipe-card.js
 
 # echo "Installing Button Card"
 # mkdir -p www/custom-lovelace/button-card
